d6aaee974267e3e58ac016b7cf2ecf0b447c582b
NeroBlackstone <905374776@qq.com>
2019-08-02 13:48:42 +0800

    update dependency

---
+++ docs/.vuepress/components/sylvannGuardCard.vue
@@ -0,0 +1,39 @@
+<template>
+    <card header >
+        <p class="title has-text-white">
+            {{title}}
+            <span>
+                <slot></slot>
+            </span>
+        </p>
+        <b-field>
+            <b-select placeholder="数字">
+                <option v-for="option in this.options">
+                    {{ option }}
+                </option>
+            </b-select>
+        </b-field>
+    </card>
+</template>
+
+<script>
+    export default {
+        name: "sylvannGuardCard",
+        props:['title'],
+        data() {
+            return {
+                options:[
+                    '- 未选择 -',
+                    '1404',
+                    '1912',
+                    '2340',
+                    '4250',
+                ]
+            }
+        },
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ docs/.vuepress/components/vodSylvannGuard.vue
@@ -0,0 +1,29 @@
+<template>
+    <four-column-layout>
+        <template #column1>
+            <sylvann-guard-card title=""></sylvann-guard-card>
+        </template>
+        <template #column2>
+            <sylvann-guard-card></sylvann-guard-card>
+        </template>
+        <template #column3>
+            <sylvann-guard-card></sylvann-guard-card>
+        </template>
+        <template #column4>
+            <sylvann-guard-card></sylvann-guard-card>
+        </template>
+    </four-column-layout>
+</template>
+
+<script>
+    import FourColumnLayout from "./fourColumnLayout";
+    import SylvannGuardCard from "./sylvannGuardCard";
+    export default {
+        name: "vodSylvannGuard",
+        components: {SylvannGuardCard, FourColumnLayout}
+    }
+</script>
+
+<style scoped>
+
+</style>
